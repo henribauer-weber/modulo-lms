cd05ea1be8a984307d15336edd3c546ca565f10a
Sujan Khadka <sujankhadka@mail.weber.edu>
2026-09-17 15:46:47 -0600

    Added the calender page Using Full Calnder

---
+++ ModuloLMS/Pages/Calendar.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ModuloLMS.Pages
+{
+    public class CalendarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
