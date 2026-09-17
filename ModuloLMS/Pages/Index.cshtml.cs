4c6afb5b3c47e6f9d0cc9e95b3f9ab5eb3c6d8b0
LaMarNP <noahperkins@mail.weber.edu>
2026-09-16 18:42:52 -0600

    Dashboard creation with small changes to layout cs to make overall scheme purple.

--- ModuloLMS/Pages/Index.cshtml.cs
+++ ModuloLMS/Pages/Index.cshtml.cs
@@ -5,9 +5,63 @@ namespace ModuloLMS.Pages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        public class CourseCardItem
         {
+            public string CourseNumber { get; set; } = string.Empty;
+            public string CourseTitle { get; set; } = string.Empty;
+            public string Instructor { get; set; } = string.Empty;
+            public string Time { get; set; } = string.Empty;
+            public string Location { get; set; } = string.Empty;
 
         }
+
+        public int NumberOfCourses { get; set; }
+        public List<CourseCardItem> EnrolledCourses { get; set; } = new();
+
+        public void OnGet()
+        {
+            EnrolledCourses = new List<CourseCardItem>
+            {
+                new CourseCardItem
+                {
+                    CourseNumber = "CS 3750",
+                    CourseTitle = "Software Engineering II",
+                    Instructor = "John Doe",
+                    Time = "MWF 10:30 AM - 11:20 AM",
+                    Location = "NOORDA Rm 302"
+                },
+                new CourseCardItem
+                {
+                    CourseNumber = "CS 3100",
+                    CourseTitle = "Operating Systems",
+                    Instructor = "Jane doe",
+                    Time = "TR 1:00 PM - 2:15 PM",
+                    Location = "Science Lab Rm 115"
+                },
+                new CourseCardItem
+                {
+                    CourseNumber = "test",
+                    CourseTitle = "test",
+                    Instructor = "test",
+                    Time = "MWF 10:30 AM - 11:20 AM",
+                    Location = "Online"
+                },
+                new CourseCardItem
+                {
+                    CourseNumber = "MATH 2250",
+                    CourseTitle = "Mathematics",
+                    Instructor = "test",
+                    Time = "MWF 10:30 AM - 11:20 AM",
+                    Location = "Traci Hall Rm 204"
+                }
+            };
+            NumberOfCourses = EnrolledCourses.Count;
+        }
+
+
+
+
+
+
     }
 }
